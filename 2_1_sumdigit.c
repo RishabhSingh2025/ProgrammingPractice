1097bcfaeaf0f5dc5c45747a9466c83b77d28fdc
RishabhSingh2025 <rishabhs97btechaiml2025@kccitm.edu.in>
2022-06-29 22:07:49 +0530

    sumdigit

---
+++ 2_1_sumdigit.c
@@ -0,0 +1,17 @@
+// Page no. 37 question no. 1.
+#include<stdio.h>
+int main()
+{
+  int num,a,b,c,d,e,sum;
+  printf("Enter a five digit number : \n");
+  scanf("%d",&num);
+  a=num%10;
+  b=(num/10)%10;
+  c=(num/100)%10;
+  d=(num/1000)%10;
+  e=(num/10000)%10;
+  sum=a+b+c+d+e;
+  printf("%d is the sum of the digits of number %d \n",sum,num);
+  return 0;
+}
+
